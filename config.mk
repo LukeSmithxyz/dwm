7faa8a904208578672160d7b57a4b8df6520581e
Anselm R. Garbe <garbeam@wmii.de>
2006-07-14 18:40:31 +0200

    prep 0.1

--- config.mk
+++ config.mk
@@ -8,18 +8,18 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-VERSION = 0.0
+VERSION = 0.1
 
 # includes and libs
 LIBS = -L${PREFIX}/lib -L/usr/lib -lc -L${X11LIB} -lX11
 
 # Linux/BSD
-#CFLAGS = -Os -I. -I${PREFIX}/include -I/usr/include -I${X11INC} \
-#	-DVERSION=\"${VERSION}\"
-#LDFLAGS = ${LIBS}
-CFLAGS = -g -Wall -O2 -I. -I${PREFIX}/include -I/usr/include -I${X11INC} \
+CFLAGS = -Os -I. -I${PREFIX}/include -I/usr/include -I${X11INC} \
 	-DVERSION=\"${VERSION}\"
-LDFLAGS = -g ${LIBS}
+LDFLAGS = ${LIBS}
+#CFLAGS = -g -Wall -O2 -I. -I${PREFIX}/include -I/usr/include -I${X11INC} \
+#	-DVERSION=\"${VERSION}\"
+#LDFLAGS = -g ${LIBS}
 
 
 # Solaris
