67b3083dfd574cf2ecba1620361108b0adeda5dc
arg@10ksloc.org <unknown>
2006-07-21 10:18:12 +0200

    s/0.5/0.6/ - my steps are wider than the reality

--- config.mk
+++ config.mk
@@ -7,7 +7,7 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-VERSION = 0.6
+VERSION = 0.5
 
 # includes and libs
 LIBS = -L${PREFIX}/lib -L/usr/lib -lc -L${X11LIB} -lX11
