4b0328f2097dedd70bbb36ea2b5159ac55b7cbcb
Anselm R.Garbe <arg@10ksloc.org>
2006-08-10 11:07:27 +0200

    drawing border with fg color

--- draw.c
+++ draw.c
@@ -28,11 +28,11 @@ drawtext(const char *text, Bool invert)
 	static char buf[256];
 	unsigned int len;
 	XGCValues gcv;
+	XPoint points[5];
 	XRectangle r = { dc.x, dc.y, dc.w, dc.h };
 
 	XSetForeground(dpy, dc.gc, invert ? dc.fg : dc.bg);
 	XFillRectangles(dpy, dc.drawable, dc.gc, &r, 1);
-
 	if(!text)
 		return;
 
@@ -66,6 +66,19 @@ drawtext(const char *text, Bool invert)
 		XChangeGC(dpy, dc.gc, GCForeground | GCBackground | GCFont, &gcv);
 		XDrawImageString(dpy, dc.drawable, dc.gc, x, y, buf, len);
 	}
+
+	XSetLineAttributes(dpy, dc.gc, 1, LineSolid, CapButt, JoinMiter);
+	points[0].x = dc.x;
+	points[0].y = dc.y;
+	points[1].x = dc.w - 1;
+	points[1].y = 0;
+	points[2].x = 0;
+	points[2].y = dc.h - 1;
+	points[3].x = -(dc.w - 1);
+	points[3].y = 0;
+	points[4].x = 0;
+	points[4].y = -(dc.h - 1);
+	XDrawLines(dpy, dc.drawable, dc.gc, points, 5, CoordModePrevious);
 }
 
 /* extern */
