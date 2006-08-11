7d4184dc5c5381d638efb9f26fd8bd6d8a6fe372
Anselm R.Garbe <arg@10ksloc.org>
2006-08-11 10:00:47 +0200

    realized that client focussing through the bar is pretty useless, better is sloppy view focussing for B1/B3 as well instead

--- event.c
+++ event.c
@@ -103,7 +103,7 @@ buttonpress(XEvent *e)
 
 	if(barwin == ev->window) {
 		switch(ev->button) {
-		case Button1:
+		default:
 			x = 0;
 			for(a.i = 0; a.i < ntags; a.i++) {
 				x += textw(tags[a.i]);
@@ -112,10 +112,10 @@ buttonpress(XEvent *e)
 					return;
 				}
 			}
-			focusprev(NULL);
-			break;
-		case Button3:
-			focusnext(NULL);
+			if(ev->button == Button1)
+				viewprev(&a);
+			else if(ev->button == Button3)
+				viewnext(&a);
 			break;
 		case Button4:
 			viewprev(&a);
