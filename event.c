3e06edeb5df0aa95614cb0b11ba1931f01b6f337
Anselm R.Garbe <arg@10ksloc.org>
2006-08-14 18:14:08 +0200

    applied sanders man page patch, removed button2 from bar click

--- event.c
+++ event.c
@@ -109,10 +109,10 @@ buttonpress(XEvent *e)
 		for(a.i = 0; a.i < ntags; a.i++) {
 			x += textw(tags[a.i]);
 			if(ev->x < x) {
-				if(ev->button == Button3)
-					toggleview(&a);
-				else
+				if(ev->button == Button1)
 					view(&a);
+				else if(ev->button == Button3)
+					toggleview(&a);
 				return;
 			}
 		}
