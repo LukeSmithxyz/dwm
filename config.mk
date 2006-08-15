b9cd0c433139ae7dac850c5cd538ee74b32797e1
Anselm R.Garbe <arg@10ksloc.org>
2006-08-15 16:54:54 +0200

    fixing small bug in config.mk

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # dwm version
-VERSION = 0.9
+VERSION = 1.0
 
 # Customize below to fit your system
 
@@ -11,7 +11,7 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
-INCS = -I/usr/lib -I${X11INC}
+INCS = -I/usr/include -I${X11INC}
 LIBS = -L/usr/lib -lc -L${X11LIB} -lX11
 
 # flags
