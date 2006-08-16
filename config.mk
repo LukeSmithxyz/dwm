14f7380308c4213aac7956c272dec0d23c2f42d3
Anselm R.Garbe <arg@10ksloc.org>
2006-08-16 12:36:32 +0200

    applied another config.mk patch made by sander

--- config.mk
+++ config.mk
@@ -11,7 +11,7 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
-INCS = -I/usr/include -I${X11INC}
+INCS = -I. -I/usr/include -I${X11INC}
 LIBS = -L/usr/lib -lc -L${X11LIB} -lX11
 
 # flags
