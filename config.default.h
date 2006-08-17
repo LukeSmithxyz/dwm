a420ceab50b896a6b6edc81c2a42c4ddc75b867e
Anselm R.Garbe <arg@10ksloc.org>
2006-08-17 09:44:17 +0200

    applied the shorter xprop command

--- config.default.h
+++ config.default.h
@@ -49,7 +49,7 @@ static Key key[] = { \
 };
 
 /* Query class:instance for regex matching info with following command:
- * xprop | grep WM_CLASS | sed 's/"//g;s/,//g' | awk '{ printf("%s:%s", $4, $3) }' */
+ * xprop | awk -F'"' '/^WM_CLASS/ { printf "%s:%s\n", $4, $2 }'*/
 #define RULES \
 static Rule rule[] = { \
 	/* class:instance regex		tags regex	isfloat */ \
