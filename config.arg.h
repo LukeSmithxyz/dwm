8c334ee078a682a4e5b01aad6b12d24cf9be26d6
Anselm R. Garbe <arg@10kloc.org>
2006-08-22 10:06:11 +0200

    rxvt is quite slow under OpenBSD

--- config.arg.h
+++ config.arg.h
@@ -20,7 +20,7 @@ const char *tags[] = { "work", "net", "fnord", NULL };
 static Key key[] = { \
 	/* modifier			key		function	arguments */ \
 	{ MODKEY|ShiftMask,		XK_Return,	spawn, \
-		{ .cmd = "exec rxvt +sb -tr -bg black -fg '#eeeeee' -fn '"TFONT"'" } }, \
+		{ .cmd = "exec uxterm +sb -bg black -fg '#eeeeee' -fn '"TFONT"'" } }, \
 	{ MODKEY,			XK_p,		spawn, \
 		{ .cmd = "exec `ls -lL /usr/bin /usr/X11R6/bin /usr/local/bin 2>/dev/null | " \
 			"awk 'NF>2 && $1 ~ /^[^d].*x/ {print $NF}' | sort -u | dmenu`" } }, \
