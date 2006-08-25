f1fe19bc2b0b1744a82551ef2580e621231cd97e
Anselm R. Garbe <arg@10kloc.org>
2006-08-25 13:02:07 +0200

    restored config.arg.h

--- config.arg.h
+++ config.arg.h
@@ -4,11 +4,11 @@
  */
 
 #define TAGS \
-const char *tags[] = { "1", "2", "3", "4", "5", NULL };
+const char *tags[] = { "work", "net", "fnord", NULL };
 
 #define DEFMODE			dotile /* dofloat */
 
-#define FONT			"fixed"
+#define FONT			"-*-terminus-medium-*-*-*-12-*-*-*-*-*-iso10646-*"
 #define SELBGCOLOR		"#666699"
 #define SELFGCOLOR		"#eeeeee"
 #define NORMBGCOLOR		"#333366"
@@ -22,41 +22,37 @@ const char *tags[] = { "1", "2", "3", "4", "5", NULL };
 #define KEYS \
 static Key key[] = { \
 	/* modifier			key		function	arguments */ \
-	{ MODKEY|ShiftMask,		XK_Return,	spawn,		{ .cmd = "exec xterm" } }, \
-	{ MODKEY,			XK_Tab,		focusnext,	{ 0 } }, \
-	{ MODKEY|ShiftMask,		XK_Tab,		focusprev,	{ 0 } }, \
+	{ MODKEY|ShiftMask,		XK_Return,	spawn, \
+		{ .cmd = "exec uxterm -bg '#e0e0e0' -fg '#000000' -cr '#000000' +sb -fn '"FONT"'" } }, \
+	{ MODKEY,			XK_p,		spawn, \
+		{ .cmd = "exec `ls -lL /usr/bin /usr/X11R6/bin /usr/local/bin 2>/dev/null | " \
+			"awk 'NF>2 && $1 ~ /^[^d].*x/ {print $NF}' | sort -u | dmenu`" } }, \
+	{ MODKEY,			XK_j,		focusnext,	{ 0 } }, \
+	{ MODKEY,			XK_k,		focusprev,	{ 0 } }, \
 	{ MODKEY,			XK_Return,	zoom,		{ 0 } }, \
 	{ MODKEY,			XK_m,		togglemax,	{ 0 } }, \
 	{ MODKEY|ShiftMask,		XK_1,		tag,		{ .i = 0 } }, \
 	{ MODKEY|ShiftMask,		XK_2,		tag,		{ .i = 1 } }, \
 	{ MODKEY|ShiftMask,		XK_3,		tag,		{ .i = 2 } }, \
-	{ MODKEY|ShiftMask,		XK_4,		tag,		{ .i = 3 } }, \
-	{ MODKEY|ShiftMask,		XK_5,		tag,		{ .i = 4 } }, \
 	{ MODKEY|ControlMask|ShiftMask,	XK_1,		toggletag,	{ .i = 0 } }, \
 	{ MODKEY|ControlMask|ShiftMask,	XK_2,		toggletag,	{ .i = 1 } }, \
 	{ MODKEY|ControlMask|ShiftMask,	XK_3,		toggletag,	{ .i = 2 } }, \
-	{ MODKEY|ControlMask|ShiftMask,	XK_4,		toggletag,	{ .i = 3 } }, \
-	{ MODKEY|ControlMask|ShiftMask,	XK_5,		toggletag,	{ .i = 4 } }, \
 	{ MODKEY|ShiftMask,		XK_c,		killclient,	{ 0 } }, \
 	{ MODKEY,			XK_space,	togglemode,	{ 0 } }, \
 	{ MODKEY,			XK_1,		view,		{ .i = 0 } }, \
 	{ MODKEY,			XK_2,		view,		{ .i = 1 } }, \
 	{ MODKEY,			XK_3,		view,		{ .i = 2 } }, \
-	{ MODKEY,			XK_4,		view,		{ .i = 3 } }, \
-	{ MODKEY,			XK_5,		view,		{ .i = 4 } }, \
 	{ MODKEY|ControlMask,		XK_1,		toggleview,	{ .i = 0 } }, \
 	{ MODKEY|ControlMask,		XK_2,		toggleview,	{ .i = 1 } }, \
 	{ MODKEY|ControlMask,		XK_3,		toggleview,	{ .i = 2 } }, \
-	{ MODKEY|ControlMask,		XK_4,		toggleview,	{ .i = 3 } }, \
-	{ MODKEY|ControlMask,		XK_5,		toggleview,	{ .i = 4 } }, \
 	{ MODKEY|ShiftMask,		XK_q,		quit,		{ 0 } }, \
 };
 
-/* Query class:instance:title for regex matching info with following command:
- * xprop | awk -F '"' '/^WM_CLASS/ { printf("%s:%s:",$4,$2) }; /^WM_NAME/{ printf("%s\n",$2) }' */
 #define RULES \
 static Rule rule[] = { \
 	/* class:instance:title regex	tags regex	isfloat */ \
-	{ "Firefox.*",			"2",		False }, \
+	{ "Firefox.*",			"net",		False }, \
 	{ "Gimp.*",			NULL,		True}, \
+	{ "MPlayer.*",			NULL,		True}, \
+	{ "Acroread.*",			NULL,		True}, \
 };
