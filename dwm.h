a1e9362d38d0527d482bf7bec735d3a949a24d82
Anselm R. Garbe <arg@10kloc.org>
2006-09-12 09:50:06 +0200

    some more simplifications of intro comment in dwm.h, this should suffice for comments in dwm

--- dwm.h
+++ dwm.h
@@ -5,30 +5,24 @@
  * dynamic window manager is designed like any other X client as well. It is
  * driven through handling X events. In contrast to other X clients, a window
  * manager like dwm selects for SubstructureRedirectMask on the root window, to
- * receive events about child window appearance and disappearance.  Only one X
+ * receive events about window appearance and disappearance.  Only one X
  * connection at a time is allowed to select for this event mask.
  *
- * Calls to fetch an X event from the event queue of the X connection are
- * blocking.  Due the fact, that dwm reads status text from standard input, a
- * select-driven main loop has been implemented which selects for reads on the
- * X connection and STDIN_FILENO to handle all data smoothly and without
- * busy-loop quirks. The event handlers of dwm are organized in an array which
- * is accessed whenever a new event has been fetched. This allows event
- * dispatching in O(1) time.
+ * Calls to fetch an X event from the X event queue connection are blocking.
+ * Due reading status text from standard input, a select-driven main loop has
+ * been implemented which selects for reads on the X connection and
+ * STDIN_FILENO to handle all data smoothly and without busy-loop quirks. The
+ * event handlers of dwm are organized in an array which is accessed whenever a
+ * new event has been fetched. This allows event dispatching in O(1) time.
  *
- * Each child window of the root window is called a client in window manager
- * terminology, except windows which have set the override_redirect flag.
- * Clients are organized in a global doubly-linked client list, the focus
- * history is remembered through a global stack list. Each client contains an
- * array of Bools of the same size as the global tags array to indicate the
- * tags of a client. There are no other data structures to organize the clients
- * in tag lists. All clients which have at least one tag enabled of the current
- * tags viewed, will be visible on the screen, all other clients are banned to
- * the x-location x + 2 * screen width.  This avoids having additional layers
- * of workspace handling.
+ * Each child of the root window is called a client, except windows which have
+ * set the override_redirect flag.  Clients are organized in a global
+ * doubly-linked client list, the focus history is remembered through a global
+ * stack list. Each client contains an array of Bools of the same size as the
+ * global tags array to indicate the tags of a client.  For each client dwm
+ * creates a small title window which is resized whenever the WM_NAME or
+ * _NET_WM_NAME properties are updated or the client is resized.
  *
- * For each client dwm creates a small title window which is resized whenever
- * the WM_NAME or _NET_WM_NAME properties are updated or the client is resized.
  * Keys and tagging rules are organized as arrays and defined in the config.h
  * file. These arrays are kept static in event.o and tag.o respectively,
  * because no other part of dwm needs access to them.  The current mode is
