1d7674bb22088c5dae110751bd57e75c141e3afb
Anselm R. Garbe <arg@10kloc.org>
2006-09-12 08:27:11 +0200

    applied Christof Musik's multihead patch for a pathologic cornercase

--- main.c
+++ main.c
@@ -225,7 +225,8 @@ xerror(Display *dpy, XErrorEvent *ee)
 	|| (ee->request_code == X_PolyFillRectangle && ee->error_code == BadDrawable)
 	|| (ee->request_code == X_PolySegment && ee->error_code == BadDrawable)
 	|| (ee->request_code == X_ConfigureWindow && ee->error_code == BadMatch)
-	|| (ee->request_code == X_GrabKey && ee->error_code == BadAccess))
+	|| (ee->request_code == X_GrabKey && ee->error_code == BadAccess)
+	|| (ee->request_code == X_CopyArea && ee->error_code == BadDrawable))
 		return 0;
 	fprintf(stderr, "dwm: fatal error: request code=%d, error code=%d\n",
 		ee->request_code, ee->error_code);
