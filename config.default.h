ec7a5ffff146d71f16d4d9de78db0f1044aa9bc9
Anselm R. Garbe <arg@suckless.org>
2007-01-05 15:04:49 +0100

    changed plus key to MODKEY-Shift-plus (US-layout restrictions ;)

--- config.default.h
+++ config.default.h
@@ -31,7 +31,7 @@ static Key key[] = { \
 	{ MODKEY,			XK_Return,	zoom,		{ 0 } }, \
 	{ MODKEY,			XK_g,		resizemaster,	{ .i = 15 } }, \
 	{ MODKEY,			XK_s,		resizemaster,	{ .i = -15 } }, \
-	{ MODKEY,			XK_plus,	incnmaster,	{ .i = 1 } }, \
+	{ MODKEY|ShiftMask,		XK_plus,	incnmaster,	{ .i = 1 } }, \
 	{ MODKEY,			XK_minus,	incnmaster,	{ .i = -1 } }, \
 	{ MODKEY|ShiftMask,		XK_0,		tag,		{ .i = -1 } }, \
 	{ MODKEY|ShiftMask,		XK_1,		tag,		{ .i = 0 } }, \
