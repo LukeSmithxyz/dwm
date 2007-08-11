2c6087e3d20b506e63d12c9f4e638ecd024e859c
Anselm R. Garbe <garbeam@gmail.com>
2007-08-11 14:44:11 +0200

    fixed a comment

--- tile.h
+++ tile.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
 /* tile.c */
-void incmaster(const char *arg);	/* arranges all windows tiled */
+void incmaster(const char *arg);	/* inc/decreases the master width */
 void tile(void);			/* arranges all windows tiled */
 void zoom(const char *arg);		/* zooms the focused client to master area, arg is ignored */
