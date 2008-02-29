da1b3fa4379acc7431eaee1331e755ef5335011b
Anselm R Garbe <garbeam@gmail.com>
2008-02-29 11:59:28 +0000

    made tag names snappier

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@
 #define SELFGCOLOR		"#ffffff"
 
 /* old */
-const char tags[][MAXTAGLEN]     = { "1", "2", "3", "4", "nil", "6", "7", "8", "www" };
+const char tags[][MAXTAGLEN]     = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 unsigned int vtags[LENGTH(tags)] = {  0 ,  0 ,  0 ,  0 ,   0  ,  1 ,  1 ,  1 ,   1   };
 
 Rule rules[] = {
