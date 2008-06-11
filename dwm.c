e5a965a27480c8b27cfb4884cc8f4988924d1087
Anselm R Garbe <garbeam@gmail.com>
2008-06-11 13:20:17 +0100

    added SHCMD support for pipe-based commands due the new spawn() versio

--- dwm.c
+++ dwm.c
@@ -51,6 +51,7 @@
 #define LENGTH(x)       (sizeof x / sizeof x[0])
 #define MAXTAGLEN       16
 #define MOUSEMASK       (BUTTONMASK|PointerMotionMask)
+#define SHCMD(cmd)      { .v = (char*[]){ "/bin/sh", "-c", cmd, NULL } }
 #define TAGMASK         ((int)((1LL << LENGTH(tags)) - 1))
 #define TEXTW(x)        (textnw(x, strlen(x)) + dc.font.height)
 
