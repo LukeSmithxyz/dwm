512541bfbd499b1c10a05aaa795967ebb5fb9493
Anselm R Garbe <garbeam@gmail.com>
2008-06-12 16:37:03 +0100

    update

--- config.def.h
+++ config.def.h
@@ -44,7 +44,7 @@ static Layout layouts[] = {
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands */
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 static Key keys[] = {
