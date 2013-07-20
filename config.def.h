4fb31e08968c5058362021887fab4bbea746aecf
Anselm R Garbe <anselm@garbe.us>
2013-07-20 09:08:46 +0200

    do not take our font declaration as default for st

--- config.def.h
+++ config.def.h
@@ -51,7 +51,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", "-f", font, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
