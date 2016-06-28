56a31dc4a7be459e3a1ea06d45427e1a4087a1a6
Hiltjo Posthuma <hiltjo@codemadness.org>
2016-06-28 18:04:56 +0200

    config.def.h: style improvement, use color Scheme enum

--- config.def.h
+++ config.def.h
@@ -12,10 +12,10 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/* fg        bg         border   */
-	{ col_gray3, col_gray1, col_gray2}, /* normal */
-	{ col_gray4, col_cyan,  col_cyan }, /* selected */
+static const char *colors[SchemeLast][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel] =  { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
