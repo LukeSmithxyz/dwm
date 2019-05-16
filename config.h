629a9c00f30b0c6bc44709d2cc95315e03cd68d3
Luke Smith <luke@lukesmith.xyz>
2019-05-16 16:33:56 -0400

    winkey

--- config.h
+++ config.h
@@ -44,7 +44,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
