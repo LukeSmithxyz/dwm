991cc10ab92caf26e9cc36c79f91a81da5d235ae
Luke Smith <luke@lukesmith.xyz>
2019-05-22 13:31:25 -0400

    F2 to quit, like in i3

--- config.h
+++ config.h
@@ -121,7 +121,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,             XK_F2,      quit,           {0} },
 };
 
 /* button definitions */
