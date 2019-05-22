4052e59daa66d088af9079e87d4acc038934a2cb
Vinicius de Oliveira <vinicius.vncy@gmail.com>
2019-05-22 20:38:19 -0300

    Adjusted misaligned borders; Ability to toggle gaps between 0 and the value of gappx set within config.h

--- config.h
+++ config.h
@@ -42,7 +42,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 #include "layouts.c"
 #include "fibonacci.c"
@@ -79,6 +79,7 @@ static Key keys[] = {
 
 	/* { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } }, */
 	/* { MODKEY,			XK_Return, spawn,          {.v = termcmd } }, */
+	{ MODKEY|ShiftMask,             XK_d,      togglegaps,     {.i = gappx } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
--- dwm.c
+++ dwm.c
@@ -204,6 +204,7 @@ static void setclientstate(Client *c, long state);
 static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
 static void setgaps(const Arg *arg);
+static void togglegaps(const Arg *arg);
 static void setlayout(const Arg *arg);
 static void setmfact(const Arg *arg);
 static void setup(void);
@@ -1535,6 +1536,16 @@ setgaps(const Arg *arg)
 		selmon->gappx = 0;
 	else
 		selmon->gappx += arg->i;
+	arrange(selmon);
+}
+
+void
+togglegaps(const Arg *arg)
+{
+	if ((selmon->gappx == 0) || (selmon->gappx + arg->i < 0))
+		selmon->gappx = arg->i;
+	else
+		selmon->gappx = 0;
 	arrange(selmon);
 }
 
