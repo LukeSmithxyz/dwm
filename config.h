a7b9a905e85479e6306d4ef391cfac9716d322da
KronikPillow <kronikpillow@gmail.com>
2019-12-17 18:27:58 +0100

    fixed misaligned borders

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 #include "layouts.c"
 #include "fibonacci.c"
