f087d20e6e60a49c756936b4312f5d194d8e63b4
Chris Down <chris@chrisdown.name>
2020-04-22 15:48:08 +0100

    getatomprop: Add forward declaration
    No functional changes, but for every other function we have a forward
    declaration here. getatomprop should be no exception.

--- dwm.c
+++ dwm.c
@@ -169,6 +169,7 @@ static void focus(Client *c);
 static void focusin(XEvent *e);
 static void focusmon(const Arg *arg);
 static void focusstack(const Arg *arg);
+static Atom getatomprop(Client *c, Atom prop);
 static int getrootptr(int *x, int *y);
 static long getstate(Window w);
 static int gettextprop(Window w, Atom atom, char *text, unsigned int size);
