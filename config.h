b9e9ed13818fa3cb18f421968bb75296157ca61c
Luke Smith <luke@lukesmith.xyz>
2020-05-05 11:47:25 -0400

    xev doesn't swallow

--- config.h
+++ config.h
@@ -47,6 +47,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   isterminal noswallow monitor */
 	{ "Gimp",     NULL,       NULL,       1 << 8,       0,           0,         0,        -1 },
 	{ "St",       NULL,       NULL,       0,            0,           1,         0,        -1 },
+	{ NULL,       NULL,       "Event Tester", 0,        0,           0,         1,        -1 },
 	{ NULL,      "spterm",    NULL,       SPTAG(0),     1,           1,         0,        -1 },
 	{ NULL,      "spcalc",      NULL,       SPTAG(1),     1,           1,         0,        -1 },
 };
