0f3f2a46e4e75615c6a73544ef91225a2888400a
Luke Smith <luke@lukesmith.xyz>
2020-05-08 13:12:08 -0400

    borders red and non-transparent

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
+static char selbordercolor[]        = "#770000";
 static char selbgcolor[]            = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
