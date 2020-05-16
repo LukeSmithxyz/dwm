af1899173a9fcafc893df35f89178e53ab7834d6
Vlad Doster <mvdoster@gmail.com>
2020-05-16 12:34:29 -0500

    Update config.h

--- config.h
+++ config.h
@@ -199,7 +199,7 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,     toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,     tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,     toggletag,      {0} },
-	{ ClkTagBar,		    0,		Button4,     shiftview,      {.i = -1} },
-	{ ClkTagBar,		    0,		Button5,     shiftview,      {.i = 1} },
-	{ ClkRootWin,		    0,		Button2,     togglebar,      {0} },
+	{ ClkTagBar,		0,		Button4,     shiftview,      {.i = -1} },
+	{ ClkTagBar,		0,		Button5,     shiftview,      {.i = 1} },
+	{ ClkRootWin,		0,		Button2,     togglebar,      {0} },
 };
