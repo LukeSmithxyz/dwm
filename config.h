6b396d0d8428092363f73d867a122150e0f72ce8
Jon Glueckstein <jglueckstein@gmail.com>
2021-03-08 08:57:59 -0500

    Added function to toggle smartgaps and bound it to Super+Shift+apostrophe.

--- config.h
+++ config.h
@@ -201,6 +201,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_semicolon,	shifttag,	{ .i = 1 } },
 	{ MODKEY,			XK_apostrophe,	togglescratch,	{.ui = 1} },
 	/* { MODKEY|ShiftMask,		XK_apostrophe,	spawn,		SHCMD("") }, */
+	{ MODKEY|ShiftMask,		XK_apostrophe,	togglesmartgaps,	{0} },
 	{ MODKEY,			XK_Return,	spawn,		{.v = termcmd } },
 	{ MODKEY|ShiftMask,		XK_Return,	togglescratch,	{.ui = 0} },
 
