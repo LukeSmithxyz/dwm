203f90bfb2d8146b61882310017a6a4fe3f13849
Luke Smith <luke@lukesmith.xyz>
2021-03-12 12:55:44 -0500

    always center

--- dwm.c
+++ dwm.c
@@ -1229,6 +1229,8 @@ manage(Window w, XWindowAttributes *wa)
 	updatewindowtype(c);
 	updatesizehints(c);
 	updatewmhints(c);
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+	c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
 	if (!c->isfloating)
