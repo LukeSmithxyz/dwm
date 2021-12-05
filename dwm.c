125a12548fec6560579a9586178804ba0cfad2f0
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2021-12-05 16:55:06 +0000

    Initial fix for sticky switching

--- dwm.c
+++ dwm.c
@@ -952,7 +952,7 @@ void
 focus(Client *c)
 {
 	if (!c || !ISVISIBLE(c))
-		for (c = selmon->stack; c && !ISVISIBLE(c); c = c->snext);
+		for (c = selmon->stack; c && (!ISVISIBLE(c) || c->issticky); c = c->snext);
 	if (selmon->sel && selmon->sel != c)
 		unfocus(selmon->sel, 0);
 	if (c) {
