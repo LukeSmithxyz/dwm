7e843c47ba573ab03a62f06638c297b9da7e5e75
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2021-12-05 17:01:20 +0000

    Fix small edge cases
    Fix case where only windows available are sticky
    Fix case where stack is empty sometimes segfaulting

--- dwm.c
+++ dwm.c
@@ -951,8 +951,13 @@ expose(XEvent *e)
 void
 focus(Client *c)
 {
-	if (!c || !ISVISIBLE(c))
-		for (c = selmon->stack; c && (!ISVISIBLE(c) || c->issticky); c = c->snext);
+	if (!c || !ISVISIBLE(c)) {
+		for (c = selmon->stack; c && (!ISVISIBLE(c) || (c->issticky && !selmon->sel->issticky)); c = c->snext);
+
+		if (!c) /* No windows found; check for available stickies */
+			for (c = selmon->stack; c && !ISVISIBLE(c); c = c->snext);
+	}
+
 	if (selmon->sel && selmon->sel != c)
 		unfocus(selmon->sel, 0);
 	if (c) {
