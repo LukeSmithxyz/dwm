adbc202e0a21d3be2a54081ead288d49d4267886
Joe Suk <ybjosuk@gmail.com>
2022-11-25 11:23:07 -0500

    change colors

--- config.h
+++ config.h
@@ -17,13 +17,13 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 0;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
-static char selbgcolor[]            = "#005577";
+static char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+/* static char normbgcolor[]           = "#222222"; */
+/* static char normbordercolor[]       = "#444444"; */
+/* static char normfgcolor[]           = "#bbbbbb"; */
+/* static char selfgcolor[]            = "#eeeeee"; */
+/* static char selbordercolor[]        = "#770000"; */
+/* static char selbgcolor[]            = "#005577"; */
 /* #include "/home/amaryllis/.cache/wal/colors-wal-dwm.h" */
 /* static char *colors[][3] = { */
         /*               fg           bg           border   */
@@ -137,9 +137,9 @@ static const char *termcmd[]  = { TERMINAL, NULL };
 
 static const char *colors[][3]      = {
     /*                     fg       bg      border */
-    [SchemeNorm]       = { gray3,   black,  gray2 },
-    [SchemeSel]        = { gray4,   blue,   blue  },
-    [SchemeTitle]      = { pink,   black,  black  }, // active window title
+    [SchemeNorm]       = { red,   black,  gray2 },
+    [SchemeSel]        = { purple,   blue,   blue  },
+    [SchemeTitle]      = { white,   black,  black  }, // active window title
     [TabSel]           = { blue,    gray2,  black },
     [TabNorm]          = { gray3,   black,  black },
     [SchemeTag]        = { gray3,   black,  black },
--- themes/catppuccin.h
+++ themes/catppuccin.h
@@ -1,12 +1,14 @@
-static const char black[]       = "#1E1D2D";
-static const char gray2[]       = "#282737"; // unfocused window border
-static const char gray3[]       = "#585767";
-static const char gray4[]       = "#282737";
-static const char blue[]        = "#96CDFB";  // focused window border
+static const char black[]       = "#6E5676";
+static const char gray2[]       = "#6E5676"; // unfocused window border
+static const char gray3[]       = "#6E5676";
+static const char gray4[]       = "#6E5676";
+static const char blue[]        = "#D87B90";  // focused window border
 static const char green[]       = "#ABE9B3";
 static const char red[]         = "#F28FAD";
 static const char orange[]      = "#F8BD96";
 static const char yellow[]      = "#FAE3B0";
 static const char pink[]        = "#d5aeea";
+static const char white[]       = "#abb2bf";
+static const char purple[]      = "#6943ff";
 static const char col_borderbar[]  = "#1E1D2D"; // inner border
 
