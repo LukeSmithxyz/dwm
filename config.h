263566a76e2e20304f0478204b48e851a94267c2
a <ybjosuk@gmail.com>
2023-02-08 21:24:50 -0500

    no bar

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static unsigned int gappoh    = 10;       /* horiz outer gap between windows and
 static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 1;        /* 0 means no bar */
+static int showbar            = 0;        /* 0 means no bar */
 static int topbar             = 0;        /* 0 means bottom bar */
 static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 /* static char normbgcolor[]           = "#222222"; */
